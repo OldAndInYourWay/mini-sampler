c4625ad524a57e7dde0734b953110a3d2aa31803
OldAndInYourWay <metroidman12@gmail.com>
2015-12-28 20:13:35 -0500

    Kit changer added
    added additional samples and allow for the change of kits

--- sampler.js
+++ sampler.js
@@ -2,7 +2,7 @@
 var AudioContextConstructor = (window.AudioContext || window.webkitAudioContext ||  window.mozAudioContext || window.oAudioContext || window.msAudioContext);
 
 var sounds = [
-              './samplecollection1/snare1.wav',
+              './samplecollection1/kick2.wav',
               './samplecollection1/snare2.wav',
               './samplecollection1/kick.wav',
               './samplecollection1/hat_opened.wav',
@@ -12,18 +12,45 @@ var sounds = [
               './samplecollection1/sound2.wav'
             ];
 
-window.onload = init;
+var kits = {
+                kit0: [
+                      './samplecollection1/kick2.wav',
+                      './samplecollection1/snare2.wav',
+                      './samplecollection1/kick.wav',
+                      './samplecollection1/hat_opened.wav',
+                      './samplecollection1/hat_closed.wav',
+                      './samplecollection1/clap.wav', 
+                      './samplecollection1/sound1.wav',
+                      './samplecollection1/sound2.wav'
+                      ],
+    
+                kit1 : ['./samplecollection2/Kick06.wav',
+                       './samplecollection2/Kit_Hat01.wav',
+                       './samplecollection2/Kit_Kick.wav',
+                       './samplecollection2/Kit_Snare01.wav',
+                       './samplecollection2/Kit_Snare06.wav',
+                       './samplecollection2/Kit_Tom01.wav',
+                       './samplecollection2/Ride06.wav',
+                       './samplecollection2/Snare_Roll01.wav']
+
+                        };
+
+window.onload = init(kits['kit0']);
+window.bpm = 300;
 var context;
 var bufferLoader;
 var sampleList;
 
-//=========== INITIALIZE SAMPLES ============
-function init() {
-  context = new AudioContext();
 
+//=========== INITIALIZE SAMPLES ============
+function init(kit) {
+  if(context !== null){
+    context = null;   
+  }
+  context = new AudioContext();
   bufferLoader = new BufferLoader(
     context,
-    sounds,
+    kit,
     finishedLoading
     );
 
@@ -52,5 +79,11 @@ function finishedLoading(bufferList) {
     sampleList = bufferList; 
 }
 
+ //=== SELECT KIT =====
+    $('.kit').click(function() {
+        init(kits[$(this).attr("id")]);
+        initScreen($(this).attr("id"));
+        initNames();
+    });
 
     
